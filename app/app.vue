484f6c8355c273e93dc6d51a16113853a788ac4c
Efril1 <ccenkcc@gmail.com>
2025-09-01 11:01:15 +0300

    log

--- app/app.vue
+++ app/app.vue
@@ -1,8 +1,12 @@
+<script setup lang="ts">
+const config = useRuntimeConfig()
+console.warn('API Key:', config.public.TMDB_API_KEY)
+</script>
 
 <template>
   <NuxtLayout>
-  <UApp>
-    <NuxtPage />
-  </UApp>
-</NuxtLayout>
+    <UApp>
+      <NuxtPage />
+    </UApp>
+  </NuxtLayout>
 </template>
