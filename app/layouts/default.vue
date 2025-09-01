4c7bca81d68067837e2d65611ffc59c0001e1d99
Efril1 <ccenkcc@gmail.com>
2025-09-01 13:55:24 +0300

    ui changes

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,36 +1,35 @@
 <template>
-    <header class="flex justify-between items-center p-3">
-      <div class="flex items-center gap-6">
-        <ULink
-          to="/"
-          class="text-xl font-bold text-gray-800 dark:text-gray-200"
-        >
-          MOVLAND
-        </ULink>
-        <ULink
-          to="/movies"
-         class="text-lg hover:text-primary"
-        >
-          Movies
-        </ULink>
-        <ULink
-          to="/series"
-          class="text-lg hover:text-primary"
-        >
-          Series
-        </ULink>
-      </div>
-  
-      <UButton
-        icon="i-lucide-github"
-        color="neutral"
-        to="https://github.com/Efril1"
-        variant="ghost"
-      />
-    </header>
-  
-    <main class="min-h-screen flex flex-col items-center p-4">
-      <slot />
-    </main>
-  </template>
-  
+  <header class="flex justify-between items-center p-3">
+    <div class="flex items-center gap-6">
+      <ULink
+        to="/"
+        class="text-2xl font-bold text-gray-800 dark:text-gray-200"
+      >
+        MOVLAND
+      </ULink>
+      <ULink
+        to="/movies"
+        class="text-lg hover:text-primary"
+      >
+        Movies
+      </ULink>
+      <ULink
+        to="/series"
+        class="text-lg hover:text-primary"
+      >
+        Series
+      </ULink>
+    </div>
+
+    <UButton
+      icon="i-lucide-github"
+      color="neutral"
+      to="https://github.com/Efril1"
+      variant="ghost"
+    />
+  </header>
+
+  <main class="min-h-screen flex flex-col items-center p-4">
+    <slot />
+  </main>
+</template>
--- app/pages/[type]/[id]/index.vue
+++ app/pages/[type]/[id]/index.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 const route = useRoute()
-
 definePageMeta({ name: 'itemid' })
 const mediaStore = useMediaStore()
+const id = Number(route.params.id)
+if (id) {
+  await mediaStore.fetchMediaById(id)
+}
 const isLoading = ref(true)
 async function initialize() {
   try {
@@ -27,14 +30,37 @@ onMounted(async () => {
       <div class="animate-spin inline-block size-6 border-3 border-current border-t-transparent text-primary rounded-full" role="status" aria-label="loading" />
     </div>
 
-    <div v-else>
-      <h1 class="text-2xl font-bold mb-2 capitalize">
-        {{ mediaStore.selectedMedia.title || mediaStore.selectedMedia.name || mediaStore.selectedMedia.original_name }}
-      </h1>
-
-      <p>Overview: {{ mediaStore.selectedMedia.overview }}</p>
-      <p>Release Date: {{ mediaStore.selectedMedia.release_date || mediaStore.selectedMedia.first_air_date }}</p>
-      <p>Rating: {{ mediaStore.selectedMedia.vote_average }}</p>
+    <div v-else class="flex gap-11 mt-20">
+      <br>
+      <img :src="`https://image.tmdb.org/t/p/original${mediaStore.selectedMedia.poster_path}`" class="flex rounded-lg w-1/5 h-1/5 object-cover">
+      <div class="flex-col max-w-2xl space-y-6">
+        <h1 class=" text-5xl font-bold mb-2 capitalize">
+          {{ mediaStore.selectedMedia.title || mediaStore.selectedMedia.name || mediaStore.selectedMedia.original_name }}
+        </h1>
+        <p class="font-bold">
+          Overview:
+        </p>
+        <p class="">
+          {{ mediaStore.selectedMedia.overview }}
+        </p>
+      </div>
+      <div class="flex w-full h-full ml-100 bg-purple-900">
+        <div class=" pl-8 flex-col max-w-2xl space-y-6 ">
+          <br>
+          <div class="flex">
+            <p class="font-bold">
+              Aired:
+            </p>
+            <span class="pl-2"> {{ mediaStore.selectedMedia.release_date || mediaStore.selectedMedia.first_air_date }}</span>
+          </div>
+          <div class="flex">
+            <p class="font-bold">
+              Rating:
+            </p>
+            <span class="pl-2"> {{ mediaStore.selectedMedia.vote_average }}</span>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
